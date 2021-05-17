1115debc7e4a3e4e8c6aadeb1c955829377c6df6
Yojhan <sryojhan@gmail.com>
2021-05-17 17:43:51 +0200

    Created idioma file

---
+++ DSIXamlProyect/DSIXamlProyect/Idioma.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSIXamlProyect
+{
+    static class Idioma
+    {
+        public enum Idiomas { Castellano, Ingles, Aleman, Esparanto }
+        public static Idiomas actual;
+
+
+        public enum Texto
+        {
+            jugar,
+            opciones,
+            idioma,
+            salir
+        }
+
+        public static string getText(Texto texto)
+        {
+            return textos[(int)actual, (int)texto];
+        }
+
+        public static string getText(Idiomas idioma, Texto texto)
+        {
+            return textos[(int)idioma, (int)texto];
+        }
+
+        public static string[,] textos = {
+            { //Español
+                "Jugar",
+                "Opciones",
+                "Idioma",
+                "Salir"
+            },
+            { //Ingles
+                "Play",
+                "Options",
+                "Language",
+                "Exit"
+            },
+            { //Aleman
+                "Adasdaskdj",
+                "asdhöS",
+                "#~@#€@",
+                "Ä'sòí"
+            },
+            { //Esperanto
+                "Ulala",
+                "Tacos",
+                "UwU",
+                "Oni chan"
+            }};
+    }
+}
