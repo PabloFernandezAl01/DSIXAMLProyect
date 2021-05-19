bcca2867206c8ee5816f9bd2f930e67c0d40c3af
ivasan07 <ivasan07@ucm.es>
2021-05-19 14:51:42 +0200

    ingame things

--- DSIXamlProyect/DSIXamlProyect/InGame.xaml.cs
+++ DSIXamlProyect/DSIXamlProyect/InGame.xaml.cs
@@ -5,12 +5,14 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
@@ -22,6 +24,8 @@ namespace DSIXamlProyect
     /// </summary>
     public sealed partial class InGame : Page
     {
+        string color = "blue";
+
         public InGame()
         {
             this.InitializeComponent();
@@ -30,6 +34,97 @@ namespace DSIXamlProyect
         private void GoToAjustes(object sender, RoutedEventArgs e)
         {
             this.Frame.Navigate(typeof(Ajustes), e);
+        }
+
+        void findNextElement(DependencyObject candidate, FocusNavigationDirection next)
+        {
+            candidate = FocusManager.FindNextFocusableElement(next);
+            if (candidate != null)
+                (candidate as Control).Focus(FocusState.Keyboard);
+        }
+
+        private void Canvas_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            DependencyObject candidate = null; //Candidato a ser el siguiente foco 
+
+            //Teclado
+            if (e.Key == VirtualKey.Up || e.Key == VirtualKey.Right || e.Key == VirtualKey.GamepadDPadDown) findNextElement(candidate, FocusNavigationDirection.Next);
+            else if (e.Key == VirtualKey.Down || e.Key == VirtualKey.Left || e.Key == VirtualKey.GamepadDPadUp) findNextElement(candidate, FocusNavigationDirection.Previous);
+            else
+            {
+                switch (e.Key)
+                {
+                    case VirtualKey.A:
+                        jugadorAzul.SetValue(Canvas.LeftProperty, Canvas.GetLeft(jugadorAzul) - 10);
+                        break;
+                    case VirtualKey.D:
+                        jugadorAzul.SetValue(Canvas.LeftProperty, Canvas.GetLeft(jugadorAzul) + 10);
+                        break;
+                    case VirtualKey.S:
+                        jugadorAzul.SetValue(Canvas.TopProperty, Canvas.GetTop(jugadorAzul) + 10);
+                        break;
+                    case VirtualKey.W:
+                        jugadorAzul.SetValue(Canvas.TopProperty, Canvas.GetTop(jugadorAzul) - 10);
+                        break;
+                }
+            }
+
+            e.Handled = true; //Activar el manejo del objeto
+        }
+
+        private void Ability_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+            if (habilidad.Visibility == Visibility.Collapsed)
+            {
+                string path = "ms-appx:///Assets/bolaAzulVacia.png";
+                Uri uri = new Uri(path);
+                bitmapImage.UriSource = uri;
+
+                bolaHabilidad.Source = bitmapImage;
+
+                habilidad.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                string path = "ms-appx:///Assets/bolaAzul.jpg";
+                Uri uri = new Uri(path);
+                bitmapImage.UriSource = uri;
+
+                bolaHabilidad.Source = bitmapImage;
+
+                habilidad.Visibility = Visibility.Collapsed;
+            }
+        }
+
+        private void Hit_Click(object sender, RoutedEventArgs e)
+        {
+            BitmapImage bitmapImage = new BitmapImage();
+
+            if (color == "red")
+            {
+                string path = "ms-appx:///Assets/tennisballblue.png";
+                Uri uri = new Uri(path);
+                bitmapImage.UriSource = uri;
+
+                jugadorRojo.Rotation = 0;
+
+                tennis.Source = bitmapImage;
+
+                color = "blue";
+            }
+            else
+            {
+                string path = "ms-appx:///Assets/tennisballred.png";
+                Uri uri = new Uri(path);
+                bitmapImage.UriSource = uri;
+
+                jugadorRojo.Rotation = -30;
+
+                tennis.Source = bitmapImage;
+
+                color = "red";
+            }
         }
     }
 }
